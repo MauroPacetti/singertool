120e071d074b2491e75f489b2334be52f68ff269
Mauro <mauro@singertool.app>
2026-04-16 22:13:20 +0200

    Photoshop-like eraser: partial stroke deletion with segment splitting

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1449168629"
+    serviceWorkerVersion: "1008221776"
   }
 });
