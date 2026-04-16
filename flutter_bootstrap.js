3df27a9d5f4f6105b169f79904c5626dfd9f4d8c
Mauro <mauro@singertool.app>
2026-04-16 21:40:27 +0200

    Add highlighter tool with colors and width slider

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3876784806"
+    serviceWorkerVersion: "2994038638"
   }
 });
