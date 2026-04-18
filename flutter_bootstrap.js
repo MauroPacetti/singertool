208a4b8ef86f73cdbf85486f4471550f7bb4bab7
Mauro <mauro@singertool.app>
2026-04-18 15:05:50 +0200

    Add DarkLyrics.com to search sources for metal lyrics

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1154850600"
+    serviceWorkerVersion: "2235425491"
   }
 });
