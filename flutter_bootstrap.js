02dc8211780aa4a14cd13c0c5c8d6fe7372df5a5
Mauro <mauro@singertool.app>
2026-04-18 14:49:22 +0200

    Clean error messages: quota, rate limit, network errors in Italian

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2779570022"
+    serviceWorkerVersion: "1154850600"
   }
 });
